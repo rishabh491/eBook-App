eb156bb282b7a665a134f3c9764b4036b838fe9b
Rishabh Sahani <132204598+rishabh491@users.noreply.github.com>
2024-03-18 18:34:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors"
 import productRoute from "./routes/productRoute.js";
 import path from "path"
+import {fileURLToPath} from 'url'
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
